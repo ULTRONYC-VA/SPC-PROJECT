0b6b2b9b0012cdf269c42d870076330f53401e51
vaibhav orgnization <manevaibhav6354@gmail.com>
2021-05-13 21:57:30 +0530

    department table added successfull with issue #5

--- Database/All tables/deoartment table.sql
+++ Database/All tables/deoartment table.sql
@@ -1,5 +1,17 @@
-
 /*
-CREATE DEPARTMENT TABLE
-*/
+-----------------------------------------------------------
+ISSUE NAME		= #5 DEPARTMENT TABLE CREATED
+CREATED BY		= VAIBHAV MANE
+CALLED BY		= VA
+DESCRICPTION	= DEPARTMENT TABLE CREATED SUCCESSFULL READY FOR COADING
+-----------------------------------------------------------
+CHANGED BY			APPROVED BY 
+VAIBHAV MANE		VA
+-----------------------------------------------------------
 
+*/
+CREATE TABLE DEPARTMET
+(	
+	D_ID INT
+	,D_NAME VARCHAR(50)
+)
